61ad1b98ba6d080baa4cdd03fd0c4bfffc2685c3
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-07-29 10:47:19 +0800

    update materi

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.get('/404', (req, res) => {
 app.get('/11', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', '11.html'));
 });
+app.get('/12', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '12.html'));
+});
 app.get('/info', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'info.html'));
 });
