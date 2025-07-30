4ed0c3f9bb0f1836daff9b93fb5d1e1be214780e
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-07-30 11:05:35 +0800

    update materi 13 revisi

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/11', (req, res) => {
 app.get('/12', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', '12.html'));
 });
-app.get('/12', (req, res) => {
+app.get('/13', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', '13.html'));
 });
 app.get('/info', (req, res) => {
